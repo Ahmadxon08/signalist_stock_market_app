ca1714186b17045a241763e23adf604c42a65c43
Ahmadxon08 <akhmadxonmoydinov@gmail.com>
2025-10-19 21:57:55 +0500

    header was added

---
+++ lib/constants.ts
@@ -0,0 +1,14 @@
+export const navItems = [
+  {
+    href: "/",
+    title: "Dashboard",
+  },
+  {
+    href: "/search",
+    title: "Search",
+  },
+  {
+    href: "/watchlist",
+    title: "Watchlist",
+  },
+];
